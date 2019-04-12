733071f6c5c6ea000afa766d783d5d3c5c29cd5b
Ethan Vizitei <evizitei@instructure.com>
2019-04-12 05:53:49 -0600

    wrap tests around writing to multiple pages

--- pkg/sql/sql_test.go
+++ pkg/sql/sql_test.go
@@ -16,6 +16,19 @@ func ClearTestFile() {
 	os.Remove(TestFileName)
 }
 
+func WriteRecords(count int, table *Table) error {
+	for i := 1; i <= count; i++ {
+		username := fmt.Sprintf("User %d", i)
+		email := fmt.Sprintf("user.%d@test.com", i)
+		row := NewRow(int32(i), username, email)
+		err := table.Append(row)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func TestPagerFileStore(t *testing.T) {
 	ClearTestFile()
 	pager := NewPager(TestFileName)
@@ -41,14 +54,9 @@ func TestReadingRowcount(t *testing.T) {
 		t.Error("Blank file should count as 0 rows")
 	}
 	rowCount := 200
-	for i := 1; i <= rowCount; i++ {
-		username := fmt.Sprintf("User %d", i)
-		email := fmt.Sprintf("user.%d@test.com", i)
-		row := NewRow(int32(i), username, email)
-		err := table.Append(row)
-		if err != nil {
-			t.Error("Error, duroing append: ", err)
-		}
+	err := WriteRecords(rowCount, table)
+	if err != nil {
+		t.Error("Failed to append records", err)
 	}
 	if table.numRows != rowCount {
 		t.Error("Did not persist correct row count: ", table.numRows)
@@ -59,4 +67,38 @@ func TestReadingRowcount(t *testing.T) {
 		t.Error("Did not recover rowcount from file: ", table.numRows)
 	}
 	ClearTestFile()
+}
+
+func TestAddressFetching(t *testing.T) {
+	ClearTestFile()
+	table := NewTable(TestFileName)
+	row1Address := table.FetchAddress(0)
+	if row1Address.PageNum != 0 || row1Address.ByteOffset != 0 {
+		t.Error("Row Offset for addressing is off: ", row1Address)
+	}
+}
+
+func TestReadingFromOtherPages(t *testing.T) {
+	ClearTestFile()
+	table := NewTable(TestFileName)
+	err := WriteRecords(30, table)
+	if err != nil {
+		t.Error("Failed to append records", err)
+	}
+	table.Close()
+	table = NewTable(TestFileName)
+	row1 := table.FetchRow(TableAddress{PageNum: 0, ByteOffset: 0})
+	if row1.ID != 1 {
+		t.Error("loaded wrong row", row1)
+	}
+	row16Address := table.FetchAddress(15)
+	if row16Address.PageNum != 1 {
+		t.Error("Address matching should have hit next page")
+	}
+	row16 := table.FetchRow(row16Address)
+	for i := 0; i <= 6; i++ {
+		if row16.Username[i] != []byte("User 16")[i] {
+			t.Error("Loaded wrong row for 16: ", string(row16.Username[0:32]))
+		}
+	}
 }
